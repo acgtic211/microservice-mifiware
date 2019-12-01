bc4e6bb25bdcc3d507e025fbde88c7f26fc77c61
Juan Alberto Llopis <p.guillen100@gmail.com>
2019-12-01 17:25:14 +0100

    Arregladas suscripciones y añadida suscripción de datos individuales.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const Entity = require('../controllers/Entity');
 const Value = require('../controllers/Value');
 const request = require('request');
 const _ = require('lodash');
+const orionUrl = "http://192.168.111.129";
 
 // If an subscription is recieved emit socket io events
 // using the attribute values from the data received to define
@@ -20,20 +21,82 @@ function broadcastEvents(req, item, types) {
   });
 }
 
+function createSubscription(paramName, paramId)
+{
+  request(orionUrl + ":1026/v2/subscriptions", function (error, response, body) {
+    if (!error && response.statusCode == 200) 
+    {
+      var reqData = JSON.parse(body);
+      var exists = false;
+      var name = paramName;
+      var queryId = '"idPattern": ".*", ';
+      
+      if(paramId != "")
+      {
+        name = paramId;
+        queryId = '"id": "' + paramId + '", ';
+      } 
+      
+      for(var i = 0; i < reqData.length; i++)
+      {
+          if(reqData[i].description == 'Notify me of all ' + name + ' changes') exists = true;
+      }
+      if(!exists)
+      {
+        request({
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          uri: orionUrl + ':1026/v2/subscriptions?options=skipInitialNotification',
+          body: '{ "description": "Notify me of all ' + name + ' changes", "subject": { "entities": [{' + queryId + '"type": "' + paramName + '"}],  "condition": { "attrs": [ ] } },  "notification": {"http": { "url": "http://192.168.2.114:3000/subscription/'+ paramName + '&' + paramId + '" } } }',
+          method: 'POST'
+        }, function (err, res, body) {
+          //it works!
+        });
+      }
+
+    } else {
+        console.log("There was an error: ") + response.statusCode;
+        console.log(body);
+        res.status(response.statusCode).send();
+    }
+  });
+}
+
 
 
 // Whenever a subscription is received, display it on the monitor
 // and notify any interested parties using Socket.io
 router.post('/subscription/:type', (req, res) => {
-  monitor.monitor('notify', req.params.type + ' received', req.body);
+  var params = req.params.type.split("&");
+  var extraUrl = "";
+  if(params[1] != "") extraUrl = "&id=" + params[1];
+
+  request(orionUrl + ":1026/v2/entities?type=" + params[0] + extraUrl +"&options=keyValues", function (error, response, body) {
+    if (!error && response.statusCode == 200) 
+    {
+       monitor.monitor(params[0], params[1], body);
+
+    } else {
+        console.log("There was an error: ") + response.statusCode;
+        console.log(body);
+        res.status(response.statusCode).send();
+    }
+});
+
+ 
   res.status(204).send();
 });
 
 router.get('/subscription', (req, res) => {
   var paramName = req.query.entity
+  var paramId = req.query.id
   var entities = [];
+  var extraUrl = "";
+
+  if(paramId != "") extraUrl = "&id=" + paramId;
   
-  request("http://192.168.183.128:1026/v2/entities?type=" + paramName + "&options=keyValues", function (error, response, body) {
+  request(orionUrl + ":1026/v2/entities?type=" + paramName + extraUrl +"&options=keyValues", function (error, response, body) {
     if (!error && response.statusCode == 200) 
     {
         var reqBody = body.split(",")
@@ -41,21 +104,12 @@ router.get('/subscription', (req, res) => {
         
         var entitiesJson = "{" + '"entities": ' + JSON.stringify(entities) + "}";
 
-        request({
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          uri: 'http://192.168.183.128:1026/v2/subscriptions',
-          body: '{ "description": "Notify me of all Room changes", "subject": { "entities": [{"idPattern": ".*", "type": "Light"}],  "condition": { "attrs": [ ] } },  "notification": {"http": { "url": "http://192.168.44.1:3000/subscription/temperature-change" } } }',
-          method: 'POST'
-        }, function (err, res, body) {
-          //it works!
-        });
-
+        createSubscription(paramName, paramId);
         res.json(JSON.parse(entitiesJson));
     } else {
         console.log("There was an error: ") + response.statusCode;
         console.log(body);
+        res.status(response.statusCode).send();
     }
 });
 });
