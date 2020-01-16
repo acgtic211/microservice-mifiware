ad1788a1d6e336adddc01ece132cd1bfde1b966b
Juan Alberto Llopis <juanalbertollopis@realtracksystems.com>
2020-01-16 14:54:22 +0100

    - Ahora se puede obtener el listado de atributos de todo el sistema y el listado de atributos de la entidad seleccionada. - Se ha adaptado el sistema para que funcione con todo el listado de entidades.

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const Entity = require('../domain/Entity');
 const Value = require('../domain/Value');
 const request = require('request');
 const _ = require('lodash');
-const orionUrl = "http://192.168.183.128";
-const microserviceUrl = "http://192.168.44.1";
+const orionUrl = "http://192.168.111.129";
+const microserviceUrl = "http://192.168.2.114";
 
 // If an subscription is recieved emit socket io events
 // using the attribute values from the data received to define
@@ -30,9 +30,15 @@ function createSubscription(paramName, paramId, paramQuery)
       var reqData = JSON.parse(body);
       var exists = false;
       var name = paramName;
+      var paramUrl = "";
       var queryId = '"idPattern": ".*", ';
       var queryFilter = '';
       var notifyFilter = '';
+
+      if(paramName != "")
+      {
+        paramUrl = '"type": "' + paramName + '"';
+      }
       
       if(paramId != "")
       {
@@ -60,7 +66,7 @@ function createSubscription(paramName, paramId, paramQuery)
             'Content-Type': 'application/json'
           },
           uri: orionUrl + ':1026/v2/subscriptions?options=skipInitialNotification',
-          body: '{ "description": "Notify me of all ' + name + ' changes", "subject": { "entities": [{' + queryId + '"type": "' + paramName + '"}],  "condition": { "attrs": [ ]' + '} },  "notification": {"http": { "url": "' + microserviceUrl + ':3000/subscription/'+ paramName + '&' + paramId + '&' + notifyFilter + '" } } }',
+          body: '{ "description": "Notify me of all ' + name + ' changes", "subject": { "entities": [{' + queryId + paramUrl + '}],  "condition": { "attrs": [ ]' + '} },  "notification": {"http": { "url": "' + microserviceUrl + ':3000/subscription/'+ paramName + '&' + paramId + '&' + notifyFilter + '" } } }',
           method: 'POST'
         }, function (err, res, body) {
           //it works!
@@ -83,22 +89,35 @@ function createSubscription(paramName, paramId, paramQuery)
 // and notify any interested parties using Socket.io
 router.post('/subscription/:type', (req, res) => {
   var params = req.params.type.split("&");
+  var paramUrl = "";
+  var paramSeparator = "?";
+  var optionsSeparator = "&";
   var extraUrl = "";
   var extraUrlQuery = '';
   var filterData = "";
+
+  if(params[0] != "")
+  {
+    paramUrl = "?type=" + params[0];
+    paramSeparator = "&";
+  }
+
+
   if(params[1] != "")
   {
-    extraUrl = "&id=" + params[1];
+    extraUrl = paramSeparator + "id=" + params[1];
   }else
   {
     if(params[2] != "" && params[2] != 'undefined')
     {
       filterData = params[2];
-      extraUrlQuery = '&q=' + decodeURIComponent(params[2]);
-    } 
+      extraUrlQuery = paramSeparator + 'q=' + decodeURIComponent(params[2]);
+    }else if(paramSeparator != "&")
+    {
+      optionsSeparator = "?";
+    }
   }
-
-  request(orionUrl + ":1026/v2/entities?type=" + params[0] + extraUrl + extraUrlQuery +"&options=keyValues", function (error, response, body) {
+  request(orionUrl + ":1026/v2/entities" + paramUrl + extraUrl + extraUrlQuery + optionsSeparator + "options=keyValues", function (error, response, body) {
     if (!error && response.statusCode == 200) 
     {
        monitor.monitor(params[0], params[1], filterData, body);
@@ -119,22 +138,33 @@ router.get('/subscription', (req, res) => {
   var paramId = req.query.id;
   var paramQuery = '';
   var entities = [];
+  var paramUrl = "";
+  var paramSeparator = "?";
+  var optionsSeparator = "&";
   var extraUrl = "";
   var extraUrlQuery = '';
 
+  if(paramName != "")
+  {
+    paramUrl = "?type=" + paramName;
+    paramSeparator = "&";
+  }
+
   if(paramId != "")
   {
-    extraUrl = "&id=" + paramId;
+    extraUrl = paramSeparator + "id=" + paramId;
   }else
   {
     if(req.query.queryFilter != 'undefined' && req.query.queryFilter != "" )
     {
       paramQuery = decodeURIComponent(req.query.queryFilter);
-      extraUrlQuery = '&q=' + decodeURIComponent(req.query.queryFilter);
-    } 
+      extraUrlQuery = paramSeparator + 'q=' + decodeURIComponent(req.query.queryFilter);
+    }else if(paramSeparator != "&")
+    {
+      optionsSeparator = "?";
+    }
   } 
-
-  request(orionUrl + ":1026/v2/entities?type=" + paramName + extraUrl + extraUrlQuery +"&options=keyValues", function (error, response, body) {
+  request(orionUrl + ":1026/v2/entities" + paramUrl + extraUrl + extraUrlQuery + optionsSeparator + "options=keyValues", function (error, response, body) {
     if (!error && response.statusCode == 200) 
     {
         var reqBody = body.split(",")
@@ -164,6 +194,67 @@ router.get('/typeList', (req, res) => {
     }
 });
 });
+
+router.get('/attributeList', (req, res) => {
+  var paramName = req.query.entity;
+  var attributeList = [];
+
+  if(paramName != "" && paramName != "undefined")
+  {
+    request(orionUrl + ":1026/v2/entities?type="+ paramName +"&options=keyValues", function (error, response, body) {
+      if (!error && response.statusCode == 200) 
+      {
+          try
+          {
+            request(orionUrl + ":1026/v2/entities/" + JSON.parse(body)[0].id + "/attrs?options=keyValues", function (error, response, body) {
+              if (!error && response.statusCode == 200) 
+              {
+                for(var i = 0; i < Object.keys(JSON.parse(body)).length; i++)
+                {
+                  attributeList.push(Object.keys(JSON.parse(body))[i])
+                }
+                  res.json(attributeList);
+              } else {
+                  console.log("There was an error: ") + response.statusCode;
+                  console.log(body);
+                  res.status(response.statusCode).send();
+              }
+          });
+          }catch(error)
+          {
+            console.log("There was an error: ") + response.statusCode;
+            console.log(body);
+            res.status(204).send();
+          }
+          
+      } else {
+        console.log("There was an error: ") + response.statusCode;
+        console.log(body);
+        res.status(response.statusCode).send();
+      }
+    });
+  }else
+  {
+    request(orionUrl + ":1026/v2/types?options=noAttrDetail", function (error, response, body) {
+      if (!error && response.statusCode == 200) 
+      {
+        for(var i = 0; i < JSON.parse(body).length; i++)
+        {
+          for(var j = 0; j < Object.keys(JSON.parse(body)[i].attrs).length; j++)
+          {
+            if(!attributeList.includes(Object.keys(JSON.parse(body)[i].attrs)[j])) attributeList.push(Object.keys(JSON.parse(body)[i].attrs)[j]);
+          }
+        }
+        res.json(attributeList);
+      } else {
+        console.log("There was an error: ") + response.statusCode;
+        console.log(body);
+        res.status(response.statusCode).send();
+      }
+    });
+  }
+});
+
 
 
 
